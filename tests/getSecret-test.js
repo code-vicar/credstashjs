735ed4b60b42251d2349f49be5d65d71a179d0f7
Scott Vickers <svickers@weareratio.com>
2016-08-25 10:33:42 -0700

    update package.json, update tests

--- tests/getSecret-test.js
+++ tests/getSecret-test.js
@@ -1,65 +1,60 @@
-var getSecret = require('../lib/getSecret')
+var proxyquire = require('proxyquire')
+var sinon = require('sinon')
 var expect = require('chai').expect
-var AWS = require('aws-sdk-mock')
 
-describe('credstash', () => {
-    afterEach(() => {
-        AWS.restore()
+describe('credstash', function () {
+    beforeEach(function () {
+        this.KMS = function () { }
+        this.KMS.prototype.decrypt = mockKMS()
+
+        this.DocumentClient = sinon.spy(function () { })
+        this.DocumentClient.prototype.query = mockQuery()
+        this.DocumentClient.prototype.get = mockGet()
+
+        this.getSecret = proxyquire('../lib/getSecret', {
+            'aws-sdk': {
+                DynamoDB: {
+                    DocumentClient: this.DocumentClient
+                },
+                KMS: this.KMS
+            }
+        })
     })
 
-    it('can get secret', () => {
-        AWS.mock('DynamoDB.DocumentClient', 'query', mockQuery('credential-store'));
-
-        AWS.mock('KMS', 'decrypt', mockKMS)
-        return getSecret('secret').then(function (secret) {
+    it('can get secret', function () {
+        return this.getSecret('secret').then((secret) => {
             expect(secret).to.equal('magic')
         })
     })
 
-    it.skip('can get secret with version', () => {
-        AWS.mock('DynamoDB.DocumentClient', 'get', mockGet('credential-store'));
-
-        AWS.mock('KMS', 'decrypt', mockKMS)
-        return getSecret('secret', '1').then(function (secret) {
+    it('can get secret with version', function () {
+        return this.getSecret('secret', '1').then((secret) => {
             expect(secret).to.equal('magic')
         })
     })
 
-    // it.skip('can get N versions of a secret', (done) => {
-    //     AWS.mock('DynamoDB', 'query', mockQueryWithTake('credential-store'))
-    //     AWS.mock('KMS', 'decrypt', mockKMS)
-    //     var credstash = new Credstash()
-    //     return credstash.get('secret', { limit: 3 }, (e, secrets) => {
-    //         should.not.exist(e)
-    //         secrets[0].should.equal('magic')
-    //         secrets[1].should.equal('magic')
-    //         secrets[2].should.equal('magic')
-    //         return done()
-    //     })
-    // })
-
-    it.skip('can get secret from alternative table', () => {
-        AWS.mock('DynamoDB', 'query', mockQuery('another-table'))
-        AWS.mock('KMS', 'decrypt', mockKMS)
-        return getSecret('secret', '_', 'another-table').then(function (secret) {
+    it('can get secret from alternative table', function () {
+        return this.getSecret('secret', '_', 'another-table').then((secret) => {
+            this.DocumentClient.calledWith({ params: { TableName: 'another-table' } })
             expect(secret).to.equal('magic')
         })
     })
 })
 
-var mockKMS = (params, done) => {
-    var ret = {
-        data: {
-            Plaintext: new Buffer('KvQ7FPrc2uYXHjW8n+Y63HHCvyRjujeaIZepV/eUkfkz8ZbM9oymmzC69+XLTlbtvRV1MNmo3ngqE+7dJHoDMw==', 'base64')
+var mockKMS = function () {
+    return function (params, done) {
+        var ret = {
+            data: {
+                Plaintext: new Buffer('KvQ7FPrc2uYXHjW8n+Y63HHCvyRjujeaIZepV/eUkfkz8ZbM9oymmzC69+XLTlbtvRV1MNmo3ngqE+7dJHoDMw==', 'base64')
+            }
         }
-    }
 
-    return done(null, ret)
+        return done(null, ret)
+    }
 }
 
-var mockQuery = (expectedTable) => {
-    return (params, done) => {
-        // expect(params.TableName).to.equal(expectedTable)
+var mockQuery = function () {
+    return function (params, done) {
         var ret = {
             data: {
                 Items: [{
@@ -74,9 +69,8 @@ var mockQuery = (expectedTable) => {
     }
 }
 
-var mockGet = (expectedTable) => {
-    return (params, done) => {
-        expect(params.TableName).to.equal(expectedTable)
+var mockGet = function () {
+    return function (params, done) {
         var ret = {
             data: {
                 Item: {
@@ -85,48 +79,6 @@ var mockGet = (expectedTable) => {
                     contents: 'H2T+k+c='
                 }
             }
-        }
-
-        return done(null, ret)
-    }
-}
-
-var mockQueryWithTake = (expectedTable) => {
-    return (params, done) => {
-        params.Limit.should.equal(3)
-        params.TableName.should.equal(expectedTable)
-        var ret = {
-            Items: [{
-                key: {
-                    S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
-                },
-                hmac: {
-                    S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
-                },
-                contents: {
-                    S: 'H2T+k+c='
-                }
-            }, {
-                    key: {
-                        S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
-                    },
-                    hmac: {
-                        S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
-                    },
-                    contents: {
-                        S: 'H2T+k+c='
-                    }
-                }, {
-                    key: {
-                        S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
-                    },
-                    hmac: {
-                        S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
-                    },
-                    contents: {
-                        S: 'H2T+k+c='
-                    }
-                }]
         }
 
         return done(null, ret)
